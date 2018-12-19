9982073d6be2d31bb409510a0e3d0d4839435f6b
Jacob Quant <jacobq@gmail.com>
2018-12-19 11:24:46 -0600

    refactor: demonstrate without needing window & console

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,20 +2,37 @@ import Controller from '@ember/controller';
 import EmberObject, { computed, observer } from '@ember/object';
 import { map } from '@ember/object/computed';
 
-window.x = EmberObject.extend({
+export default Controller.extend({
+  init() {
+    this._super(...arguments);
+    this.set('messages', []);
+  },
+  log(message) {
+    // eslint-disable-next-line no-console
+    console.log(message);
+    this.messages.pushObject(`${Date.now()}: ${message}`);
+  },
+
+  a: null,
   x: map('a', function(a) {
     return EmberObject.create(a);
   }),
   y: computed('x.@each.{a,b}', function() {
-    // eslint-disable-next-line no-console
-    console.log('computing y');
+    this.log('computing y; returning x[0]');
     return this.x[0];
   }),
   _a: observer('y', function() {
+    this.log('_a (observer) firing; returning y');
     return this.y;
   }),
-}).create();
 
-export default Controller.extend({
-  zzz: window.x,
+  actions: {
+    setA() {
+      this.log('setA action running');
+      this.set('a', [{
+        a: 1,
+        b: 2
+      }]);
+    }
+  }
 })
