cda932beb122c6b9d9092eea06c8a672153e9532
Jacob Quant <jacobq@gmail.com>
2018-12-19 12:53:12 -0600

    feat: rewrite to illustrate problem better

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import { computed, observer } from '@ember/object';
-
+import { scheduleOnce } from '@ember/runloop';
 
 export default Controller.extend({
   init() {
@@ -8,8 +8,16 @@ export default Controller.extend({
     this._reset();
   },
   _reset() {
-    this.set('a', []);
-    this.set('messages', []);
+    this.setProperties({
+      a: [],
+      messages: [],
+      counts: {
+        a: 0,
+        e: 0,
+        de: 0,
+        de2: 0,
+      }
+    });
   },
   log(message) {
     // eslint-disable-next-line no-console
@@ -17,13 +25,48 @@ export default Controller.extend({
     this.messages.pushObject(`${Date.now()}: ${message}`);
   },
 
-  y: computed('a.@each.{a,b}', function() {
-    this.log('computing y; returning a[0]');
+  _inc(counter) {
+    scheduleOnce('afterRender', () => {
+      this.incrementProperty(counter);
+    })
+  },
+
+  arrayCP: computed('a.[]', function() {
+    this.log('arrayCP computing');
+    this._inc('counts.a');
     return this.a.firstObject;
   }),
-  _a: observer('y', function() {
-    this.log('_a (observer) firing; returning y');
-    return this.y;
+  eachCP: computed('a.@each.a', function() {
+    this.log('eachCP computing');
+    this._inc('counts.e');
+    return this.a.firstObject;
+  }),
+  doubleEachCP: computed('a.@each.{a,b}', function() {
+    this.log('doubleEachCP computing');
+    this._inc('counts.de');
+    return this.a.firstObject;
+  }),
+  doubleEachCP2: computed('a.@each.{a,b}', function() {
+    this.log('doubleEachCP2 computing');
+    this._inc('counts.de2');
+    return this.a.firstObject;
+  }),
+
+  _arrayCPObserver: observer('arrayCP', function() {
+    this.log('_arrayCPObserver firing');
+    return this.arrayCP;
+  }),
+  _eachCPObserver: observer('eachCP', function() {
+    this.log('_eachCPObserver firing');
+    return this.eachCP;
+  }),
+  _doubleEachCPObserver: observer('doubleEachCP', function() {
+    this.log('_doubleEachCPObserver firing');
+    return this.doubleEachCP; 
+  }),
+  _doubleEachCPObserverWithoutReturn: observer('doubleEachCP2', function() {
+    this.log('_doubleEach2CPObserver firing');
+    // no return value here
   }),
 
   actions: {
